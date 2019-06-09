22991a3e695d863234a13890f4024e462455a9aa
Ubuntu <ubuntu@ip-172-31-82-220.ec2.internal>
2019-06-09 13:30:15 +0000

    Set up basic wiring

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "blog_project"
+app.config["MONGO_URI"] = "mongodb+srv://root:maniac93@myfirstcluster-ilypv.mongodb.net/blog_project?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello world"
+@app.route("/publish_article")
+def publish_article():
+    return render_template("articles.html", articles=mongo.db.articles.find())
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
 
